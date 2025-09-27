e177d0f84fad5baefaf61667718fc3961f205fdb
arshia <arshia21420@email.com>
2025-09-27 21:09:58 +0330

    product list added / product list pagination added / start product detail ...

--- shop_module/views.py
+++ shop_module/views.py
@@ -5,5 +5,5 @@ from .models import Product
 class ProductListView(ListView):
     queryset = Product.objects.published()
     template_name = 'shop_module/product_list.html'
-    paginate_by = 12
+    paginate_by = 1
     context_object_name = 'products'
