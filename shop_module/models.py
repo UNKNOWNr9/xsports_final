777959675280113da445674f7f03dd66a41efa8f
ارشیا صمیمی فر <A.Samimifar@tav.local>
2025-09-27 13:32:46 +0330

    product_model added

--- shop_module/models.py
+++ shop_module/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Color(models.Model):
+    title = models.CharField(max_length=20, verbose_name='عنوان')
+
+    class Meta:
+        verbose_name = 'رنگ بندی'
+        verbose_name_plural = 'رنگ بندی ها'
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100, verbose_name='نام محصول')
+    body = models.TextField(verbose_name='توضیحات محصولات')
+    slug = models.SlugField(max_length=50, unique=True, blank=True, editable=False, verbose_name='آدرس')
+    price = models.IntegerField(max_length=10, verbose_name='قیمت')
+    image = models.ImageField(upload_to='products', verbose_name='تصویر')
+    color = models.ManyToManyField(Color, related_name='products', verbose_name='رنگ')
+    discount = models.PositiveSmallIntegerField(default=0, verbose_name='تخفیف')
+    stock = models.PositiveIntegerField(default=0, verbose_name='موجودی')
+    is_active = models.BooleanField(verbose_name='فعال / غیرفعال')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='تاریخ آپدیت')
+
+    class Meta:
+        verbose_name = 'محصول'
+        verbose_name_plural = 'محصولات'
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = self.title.replace(' ', '+')
+            slug = base_slug
+            counter = 1
+            while Product.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            self.slug = slug
+        super().save(*args, **kwargs)
--- shop_module/tests.py
+++ shop_module/tests.py
@@ -1,3 +1,4 @@
 from django.test import TestCase
 
 # Create your tests here.
+print(len('لپ تاپ 15.6 اینچی لنوو مدل LOQ 15ARP9-R7 7435HS-24GB DDR5 4800MHz-512GB SSD-RTX4050 6GB-FHD 144Hz-W - کاستوم شده'))
